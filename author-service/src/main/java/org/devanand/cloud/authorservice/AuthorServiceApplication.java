e98485f0beaf33de89f98b428f98646c72db13f4
Anand <anandhuatwork@gmail.com>
2018-12-17 16:57:40 -0500

    initial commit

---
+++ author-service/src/main/java/org/devanand/cloud/authorservice/AuthorServiceApplication.java
@@ -0,0 +1,16 @@
+package org.devanand.cloud.authorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorServiceApplication.class, args);
+	}
+
+}
+
